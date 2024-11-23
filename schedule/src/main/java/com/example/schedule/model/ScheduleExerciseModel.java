f4c8737e07fde3a3548b17c77f743cb0ae8aec94
DESKTOP-PJL516Q <nethupama1234@gmail.com>
2024-11-23 16:11:47 +0530

    shedules get request

--- schedule/src/main/java/com/example/schedule/model/ScheduleExerciseModel.java
+++ schedule/src/main/java/com/example/schedule/model/ScheduleExerciseModel.java
@@ -1,9 +1,6 @@
 package com.example.schedule.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,4 +18,5 @@ public class ScheduleExerciseModel {
     private int scheduleId;
     private int reps;
     private int sets;
+
 }
--- schedule/src/main/java/com/example/schedule/repo/ScheduleRepo.java
+++ schedule/src/main/java/com/example/schedule/repo/ScheduleRepo.java
@@ -2,8 +2,22 @@ package com.example.schedule.repo;
 
 import com.example.schedule.model.ScheduleModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 public interface ScheduleRepo extends JpaRepository<ScheduleModel,Integer> {
+    @Modifying
+    @Query(value = "UPDATE schedule_model SET is_active=false WHERE is_active=true AND member_id=?1 AND schedule_type=?2", nativeQuery = true)
+    Integer setIsActiveFalse(Integer memberId,String scheduleType);
+
+    @Query(value = "SELECT schedule_id FROM schedule_model WHERE is_active=true AND member_id=?1 AND schedule_type=?2", nativeQuery = true)
+    List<Integer> findFirstSchedules(Integer memberId,String scheduleType);
+
+    @Query(value="SELECT schedule_id,member_id,schedule_type,start_date,is_active FROM schedule_model WHERE member_id=?1 AND is_active=?2",nativeQuery = true)
+    List<ScheduleModel> getSchedulesById(Integer memberId,boolean isActive);
+
 }

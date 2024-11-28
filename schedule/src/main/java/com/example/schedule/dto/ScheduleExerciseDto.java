d11b4ca4276852bf87c1d56293da6fee5fd91479
DESKTOP-PJL516Q <nethupama1234@gmail.com>
2024-11-28 22:35:43 +0530

    adding multiple instance for a service using eureka

--- schedule/src/main/java/com/example/schedule/dto/ScheduleExerciseDto.java
+++ schedule/src/main/java/com/example/schedule/dto/ScheduleExerciseDto.java
@@ -1,10 +1,9 @@
 package com.example.schedule.dto;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import java.util.List;
+
 
 @Data
 @NoArgsConstructor

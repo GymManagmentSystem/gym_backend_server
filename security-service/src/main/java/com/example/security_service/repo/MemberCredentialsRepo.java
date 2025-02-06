17cec12dc420082b11d2f300d3fb972606751afd
DESKTOP-PJL516Q <nethupama1234@gmail.com>
2025-02-07 00:50:46 +0530

    adding Password Reset

--- security-service/src/main/java/com/example/security_service/repo/MemberCredentialsRepo.java
+++ security-service/src/main/java/com/example/security_service/repo/MemberCredentialsRepo.java
@@ -1,10 +1,19 @@
 package com.example.security_service.repo;
 
 import com.example.security_service.entity.MemberCredentialsModel;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MemberCredentialsRepo extends JpaRepository<MemberCredentialsModel,Integer> {
     @Query(value = "SELECT user_name,password,member_id,is_first_user from member_credentials_model WHERE user_name=?1",nativeQuery = true)
     MemberCredentialsModel findByUserName(String userName);
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE member_credentials_model SET password=:password WHERE user_name=:userName",nativeQuery = true)
+    Integer resetPassword(@Param("password") String password,@Param("userName") String userName);
+
 }

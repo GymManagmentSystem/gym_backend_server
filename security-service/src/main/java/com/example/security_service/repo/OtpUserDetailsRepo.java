ec5473164b5bf4466629a8a5b1eed06d70d4a941
DESKTOP-PJL516Q <nethupama1234@gmail.com>
2025-02-07 20:37:30 +0530

    adding validating otp

--- security-service/src/main/java/com/example/security_service/repo/OtpUserDetailsRepo.java
+++ security-service/src/main/java/com/example/security_service/repo/OtpUserDetailsRepo.java
@@ -1,9 +1,22 @@
 package com.example.security_service.repo;
 
+import com.example.security_service.entity.MemberCredentialsModel;
 import com.example.security_service.entity.OtpUserDetailsModel;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OtpUserDetailsRepo extends JpaRepository<OtpUserDetailsModel,String> {
+    @Query(value = "SELECT * FROM otp_user_details_model WHERE email=?1",nativeQuery = true)
+    OtpUserDetailsModel findByEmail(String email);
+
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM otp_user_details_model WHERE email=:email",nativeQuery = true)
+    void deleteOtpRecord(@Param("email") String email);
+
 }
